7446620442b0a5e85e3446d66402b7aab72878c6
YuriiTrofimenko <tyaa@ukr.net>
2019-11-30 18:12:45 +0200

    init

--- LINQtoObjectSimpleDemo/Program.cs
+++ LINQtoObjectSimpleDemo/Program.cs
@@ -10,6 +10,40 @@ namespace LINQtoObjectSimpleDemo
     {
         static void Main(string[] args)
         {
+            /* List<int> integers = new List<int>() { 1, 100, -4, 56, 876, 0, -10, 900 };
+            var filteredIntegers =
+                integers
+                .Where((integer) => integer > 0)
+                //.OrderBy((integer) => integer)
+                .OrderByDescending((integer) => integer)
+                .Skip(2)
+                .Take(2)
+                .ToList();
+            filteredIntegers.ForEach(Console.WriteLine); */
+
+            List<Person> people =
+                new List<Person>() {
+                    new Person(){ Name = "John", Age = 15 },
+                    new Person(){ Name = "Bill", Age = 55 },
+                    new Person(){ Name = "Sarah", Age = 20 }
+                };
+            List<double> salaries =
+                new List<double>() { 10000, 12000, 40000, 56000 };
+            var filteredPeople =
+                people
+                .Select((p) => p.Name)
+                .Zip(salaries, (personName, salary) => new { Name = personName, Salary = salary })
+                .ToList();
+            // filteredPeople.ForEach(Console.WriteLine);
+            /*foreach (var item in filteredPeople)
+            {
+                Console.WriteLine(item.GetType());
+            }*/
+            var filteredPeopleExpression =
+                people
+                .Select((p) => p.Name)
+                .Zip(salaries, (personName, salary) => new { Name = personName, Salary = salary });
+            Console.WriteLine(filteredPeopleExpression);
         }
     }
 }
