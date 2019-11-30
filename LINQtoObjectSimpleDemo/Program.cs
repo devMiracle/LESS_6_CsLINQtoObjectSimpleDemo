9fdc2ab931ef9e5e7d04b73d5f660d6a1cb33508
YuriiTrofimenko <tyaa@ukr.net>
2019-11-30 17:33:08 +0200

    Добавьте файлы проекта.

---
+++ LINQtoObjectSimpleDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQtoObjectSimpleDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
